e883e708f3b1ebc761fd9bd4a2fd8f5bd5727cc4
UrbanCee <urban.cee@gmx.net>
2024-12-15 14:59:18 +0100

    day15

--- day15.cpp
+++ day15.cpp
@@ -19,7 +19,19 @@ void outputMap(const std::vector<char> &map, const Coord &c)
     }
 }
 
-void moveBot(std::vector<char> &map,const Coord &c,Vec &bot,char cdir)
+void outputMap2(const std::vector<char> &map, const Coord &c, const Vec &bot)
+{
+    for (int y=0;y<c.height;y++)
+    {
+        std::string line;
+        std::copy(map.begin()+(y*c.width),map.begin()+((y+1)*c.width),std::back_inserter(line));
+        if (y==bot.y)
+            line.at(bot.x)='@';
+        std::cout << line << std::endl;
+    }
+}
+
+Vec cdir2dir(char cdir)
 {
     Vec dir;
     switch(cdir)
@@ -29,6 +41,12 @@ void moveBot(std::vector<char> &map,const Coord &c,Vec &bot,char cdir)
     case 'v':dir=Vec(0,1);break;
     case '<':dir=Vec(-1,0);break;
     }
+    return std::move(dir);
+}
+
+void moveBot(std::vector<char> &map,const Coord &c,Vec &bot,char cdir)
+{
+    Vec dir=cdir2dir(cdir);
     int distToFree=0;
     int lookIndex=0;
     while(distToFree==0){
@@ -38,19 +56,73 @@ void moveBot(std::vector<char> &map,const Coord &c,Vec &bot,char cdir)
         if (currentMapChar=='.') distToFree=lookIndex;
     }
     if (distToFree==0) return;
-    for (int i=distToFree;i>1;i--)
+    for (int i=distToFree;i>0;i--)
     {
-        map.at(c.toIndex(bot+i*dir))='O';
+        map.at(c.toIndex(bot+i*dir))=map.at(c.toIndex(bot+(i-1)*dir));
     }
     bot = bot + dir;
     map.at(c.toIndex(bot))='.';
 }
 
-long long score(const std::vector<char> &map,const Coord &c){
+bool moveDoubleBoxVert(std::vector<char> &map, const Coord &c, const Vec &pos, const Vec&dir, bool checkOnly){
+    if (map.at(c.toIndex(pos+dir))=='.') return true;
+    //std::cout << "movecall" << std::endl;
+    //outputMap2(map,c,pos);
+    int indexoffset = map.at(c.toIndex(pos+dir))=='['?1:-1;
+    if (indexoffset==-1 && !(map.at(c.toIndex(pos+dir))==']')) std::cout << "error char: " << map.at(c.toIndex(pos+dir)) << std::endl;
+    if (map.at(c.toIndex(pos+2*dir))=='#' || map.at(c.toIndex(pos+2*dir)+indexoffset)=='#')
+        return false;
+    bool leftOk = map.at(c.toIndex(pos+2*dir))=='.';
+    bool rightOk = map.at(c.toIndex(pos+2*dir)+indexoffset)=='.';
+    if (leftOk && rightOk){
+        if (!checkOnly){
+            map.at(c.toIndex(pos+2*dir))=indexoffset>0?'[':']';
+            map.at(c.toIndex(pos+2*dir)+indexoffset)=indexoffset>0?']':'[';
+            map.at(c.toIndex(pos+dir))='.';
+            map.at(c.toIndex(pos+dir)+indexoffset)='.';
+            //std::cout << "after movement:" << std::endl;
+            //outputMap2(map,c,pos);
+        }
+        return true;
+    }
+    leftOk = leftOk || moveDoubleBoxVert(map,c,pos+dir,dir,checkOnly);
+    rightOk = rightOk || moveDoubleBoxVert(map,c,c.toVec(c.toIndex(pos+dir)+indexoffset),dir,checkOnly);
+    if (checkOnly)
+        return leftOk && rightOk;
+    else{
+        if (!rightOk || !leftOk)
+            std::cout << "error:" << std::endl;
+        else{
+            //std::cout << "map before recursion:" << std::endl;
+            //outputMap2(map,c,pos);
+            return moveDoubleBoxVert(map,c,pos,dir,false);
+        }
+        return false;
+    }
+}
+
+void moveBot2(std::vector<char> &map,const Coord &c,Vec &bot,char cdir)
+{
+    if (cdir=='<' || cdir=='>') return moveBot(map,c,bot,cdir);
+    Vec dir=cdir2dir(cdir);
+    if (map.at(c.toIndex(bot+dir))=='#') return;
+    if (map.at(c.toIndex(bot+dir))=='.') {
+        bot = bot + dir;
+        return;
+    }
+    if (moveDoubleBoxVert(map,c,bot,dir,true)){
+        moveDoubleBoxVert(map,c,bot,dir,false);
+        bot = bot + dir;
+        return;
+    }
+
+}
+
+long long score(const std::vector<char> &map,const Coord &c,char scoreChar = 'O'){
     long long score =0;
     for (int i=0;i<map.size();i++)
     {
-        if (map.at(i)=='O')
+        if (map.at(i)==scoreChar)
         {
             score+=c.x(i)+c.y(i)*100;
         }
@@ -80,13 +152,33 @@ void day15()
     auto it = std::find(map.begin(),map.end(),'@');
     Vec bot = c.toVec(std::distance(map.begin(),it));
     *it = '.';
-    outputMap(map,c);
+    Vec bot2 = bot;
+    std::vector<char> map2;
+    bot2.x*=2;
+    for(auto &&ch: map)
+    {
+        if (ch=='O'){
+            map2.push_back('[');
+            map2.push_back(']');
+        }else{
+            map2.push_back(ch);
+            map2.push_back(ch);
+        }
+    }
     for (auto &&dir : dirs)
     {
         moveBot(map,c,bot,dir);
     }
+    Coord c2(c.width*2,c.height);
+    for (auto &&dir : dirs)
+    {
+        //std::cout << dir << std::endl;
+        moveBot2(map2,c2,bot2,dir);
+        //outputMap2(map2,c2,bot2);
+        //std::cout << std::endl;
+    }
 
 
     std::cout << "Day15 task1: " << score(map,c) << std::endl;
-    std::cout << "Day15 task2: " << std::endl;
+    std::cout << "Day15 task2: " << score(map2,c2,'[') << std::endl;
 }
