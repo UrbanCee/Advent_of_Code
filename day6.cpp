15f0d9aa594399364e3a6953ab1e9b3831de8a31
UrbanCee <urban.cee@gmx.net>
2024-12-18 12:43:46 +0100

    day18 _ 1

--- day6.cpp
+++ day6.cpp
@@ -13,7 +13,9 @@
 void day6()
 {
     std::ifstream inputFile(std::filesystem::path("../../inputs/day6_training.txt"));
+    Coord c(7,7);
     //std::ifstream inputFile(std::filesystem::path("../../inputs/day6.txt"));
+    //Coord c(71,71);
     for (std::string line; std::getline(inputFile,line);)
     {
         std::istringstream in(line);
