e18c56373cc6546863a63df59744baf5a5fd43eb
UrbanCee <urban.cee@gmx.net>
2025-02-05 16:25:43 +0100

    day1 python

--- python/2024/day1.py
+++ python/2024/day1.py
@@ -1,9 +1,15 @@
 
 list 
 
-with open("inputs/day1_training.txt","rt") as fp:
+with open("inputs/day1.txt","rt") as fp:
     lines = [list(map(int,line.split())) for line in fp.readlines()]
-    
-for out in lines:
-    print(out)    
+
+left = [x[0] for x in lines]
+right = [x[1] for x in lines]
+
+print("Task1: ",sum([abs(x-y) for x,y in zip(sorted(left),sorted(right))]))
+print("Task2: ",sum([x*right.count(x) for x in left]))
+
+
+  
         
