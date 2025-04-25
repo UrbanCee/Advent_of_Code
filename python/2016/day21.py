5f4c5f428e7a5bb085c21470e4a377fc7f09903c
UrbanCee <urban.cee@gmx.net>
2025-04-25 09:46:16 +0200

    update

---
+++ python/2016/day21.py
@@ -0,0 +1,36 @@
+import re
+
+swpPosPat=re.compile(r'swap position (\d+) with position (\d+)')
+swpLetPat=re.compile(r'swap letter (\w+) with letter (\w+)')
+rotatePat=re.compile(r'rotate (left|right) (\d+) steps*')
+rotatePosPat=re.compile(r'rotate based on position of letter (\w+)')
+reversePat=re.compile(r'reverse positions (\d+) through (\d+)')
+movePat=re.compile(r'move position (\d+) to position (\d+)')
+
+
+with open("inputs/day21_train.txt") as fp:
+    cmds = list(map(str.strip,fp.readlines()))
+    
+code = "abcde"
+
+def swp(code,X,Y):
+    if X<Y: code=code[:X]+code[Y]+code[X+1:Y]+code[X]+code[Y+1:]
+    else: code=code[:Y]+code[X]+code[Y+1:X]+code[Y]+code[X+1:]
+    
+
+count = 0
+for cmd in cmds:
+    for X,Y in [(int(X),int(Y)) for X,Y in swpPosPat.findall(cmd)]:
+        code=swp(code,X,Y)
+    for cx,cy in swpLetPat.findall(cmd):
+        code=swp(code,code.find(cx),code.find(cy))
+    for dir,steps in rotatePat.findall(cmd):
+        count+=1
+    for letter in rotatePosPat.findall(cmd):
+        count+=1
+    for X,Y in reversePat.findall(cmd):
+        count+=1
+    for X,Y in movePat.findall(cmd):
+        count+=1
+        
+print(code)
