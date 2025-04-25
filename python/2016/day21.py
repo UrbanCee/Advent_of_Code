220c38027df41339fb270139ffe5bdc15b021959
UrbanCee <urban.cee@gmx.net>
2025-04-25 10:38:31 +0200

    rotate

--- python/2016/day21.py
+++ python/2016/day21.py
@@ -14,8 +14,8 @@ with open("inputs/day21_train.txt") as fp:
 code = "abcde"
 
 def swp(code,X,Y):
-    if X<Y: code=code[:X]+code[Y]+code[X+1:Y]+code[X]+code[Y+1:]
-    else: code=code[:Y]+code[X]+code[Y+1:X]+code[Y]+code[X+1:]
+    if X<Y: return code[:X]+code[Y]+code[X+1:Y]+code[X]+code[Y+1:]
+    else: return code[:Y]+code[X]+code[Y+1:X]+code[Y]+code[X+1:]
     
 
 count = 0
@@ -25,12 +25,12 @@ for cmd in cmds:
     for cx,cy in swpLetPat.findall(cmd):
         code=swp(code,code.find(cx),code.find(cy))
     for dir,steps in rotatePat.findall(cmd):
-        count+=1
+        if dir=="left": code = "".join([code[(i+int(steps))%len(code)] for i in range(len(code))])
+        else: code = "".join([code[(i+len(code)-int(steps))%len(code)] for i in range(len(code))])
     for letter in rotatePosPat.findall(cmd):
         count+=1
     for X,Y in reversePat.findall(cmd):
         count+=1
     for X,Y in movePat.findall(cmd):
         count+=1
-        
-print(code)
+
