ffcb61b8bb666066d67a5143a3d02cc141610205
UrbanCee <urban.cee@gmx.net>
2025-04-28 13:12:38 +0200

    2015 24

--- python/2015/day24.py
+++ python/2015/day24.py
@@ -1,21 +1,15 @@
 from itertools import combinations
+import math
 
 with open("inputs/day24.txt") as fp:
     weights = sorted([int(line) for line in fp.readlines() if len(line)>0],reverse=True)
 
-def mul(weights):
-    result=1
-    for el in weights:
-        result*=el
-    return result
-
 def calc(numberOfGroups):
-    groupWeight=sum(weights)//numberOfGroups
-    for groupSize in range(len(weights)):
-        possibleLists=[x for x in combinations(weights,groupSize) if sum(x)==groupWeight]
-        if len(possibleLists)>0:
-            break
-    print("Task: ",min([mul(weights) for weights in possibleLists]))
+    possibleLists,groupSize=[],1
+    while len(possibleLists)==0:
+        possibleLists=[x for x in combinations(weights,groupSize) if sum(x)==sum(weights)//numberOfGroups]
+        groupSize+=1
+    return min([math.prod(fstGroupWeights) for fstGroupWeights in possibleLists])
 
-calc(3)
-calc(4)
+print("Task 1:",calc(3))
+print("Task 2:",calc(4))
