34e6ab87bf8efa4d2c9ef6b0734e76d52b12e543
UrbanCee <urban.cee@gmx.net>
2025-05-01 14:46:11 +0200

    day11 better

--- python/2017/day11.py
+++ python/2017/day11.py
@@ -1,14 +1,14 @@
 from size import *
+import itertools as it
+import functools as fc
 
 with open("inputs/day11.txt") as fp:
     path = fp.read().strip().split(",")
 
 dirs={"ne":(1,1),"n":(0,2),"nw":(-1,1),"se":(1,-1),"s":(0,-2),"sw":(-1,-1)}
-maxdist=0
-pos=(0,0)
-for dir in path:
-    pos=vAdd(pos,dirs[dir])
-    maxdist=max(maxdist,abs(pos[0])+max(0,(abs(pos[1])-abs(pos[0]))//2))
 
-print("Task1: ",abs(pos[0])+max(0,(abs(pos[1])-abs(pos[0]))//2))
-print("Task2: ",maxdist)
+def distToZero(pos):
+    return abs(pos[0])+max(0,(abs(pos[1])-abs(pos[0]))//2)
+
+print("Task1: ",distToZero(fc.reduce(vAdd,[dirs[x] for x in path])))
+print("Task2: ",max([distToZero(pos) for pos in it.accumulate([dirs[x] for x in path],vAdd)]))
