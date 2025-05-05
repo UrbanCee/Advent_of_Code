1e7c9068a1222da2f5d03433fb27cabd38ace9f7
UrbanCee <urban.cee@gmx.net>
2025-05-05 11:51:35 +0200

    Update day20.py

--- python/2015/day20.py
+++ python/2015/day20.py
@@ -1,23 +1,19 @@
 import math
-import numpy as np
 
 def divisors(house):
     lowElves = [i for i in range(1,int(math.sqrt(house))+1) if house%i==0]
-    highElves = [house//i for i in lowElves if i*i!=house]
-    return lowElves+highElves
+    return lowElves+[house//i for i in lowElves if i*i!=house]
 
-task1, task2 = 0,0
-houses=1
-targetPresents=34000000
+task,houses,targetPresents = [0,0],1,34000000
 
-while task1==0 or task2==0:
+while task[0]*task[1]==0:
     divs=divisors(houses)
-    if task1==0:
-        if 10*np.sum([x for x in divs])>=targetPresents:
-            task1=houses
-    if task2==0:
-        if 11*np.sum([x for x in divs if x*50>=houses])>=targetPresents:
-            task2=houses
+    if task[0]==0:
+        if 10*sum([x for x in divs])>=targetPresents:
+            task[0]=houses
+    if task[1]==0:
+        if 11*sum([x for x in divs if x*50>=houses])>=targetPresents:
+            task[1]=houses
     houses+=1
-print("Task1: ",task1)
-print("Task2: ",task2)
+print("Task1: ",task[0])
+print("Task2: ",task[1])
