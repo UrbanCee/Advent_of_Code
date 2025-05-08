94c6d3a53a900df39a7d369d87094438200f05e9
UrbanCee <urban.cee@gmx.net>
2025-05-08 13:50:32 +0200

    day22 finally

--- python/2015/day22.py
+++ python/2015/day22.py
@@ -9,14 +9,12 @@ spells = [
     ("re"  ,229 ,5   ,0   ,0   ,0   ,101 )
 ]
 
-init_bossHP =  71
-bossdmg = 10
-init_playerHP = 50
-init_mana = 500
+init_bossHP,bossdmg =  71,10
+init_playerHP,init_mana = 50,500
 
 bestresult=100000000
 
-def fight(bossHP,playerHP,mana,bossturn,effects,manaspent,castSpells):
+def fight(bossHP,playerHP,mana,bossturn,effects,manaspent,castSpells,loseHP):
     global bestresult
     stillActiveEffects={}
     playerAr=0
@@ -25,30 +23,34 @@ def fight(bossHP,playerHP,mana,bossturn,effects,manaspent,castSpells):
         playerHP+=v[4]
         playerAr+=v[5]
         mana+=v[6]
-        if v[2]>0:
+        if v[2]-1>0:
             stillActiveEffects[k]=(v[0],v[1],v[2]-1,v[3],v[4],v[5],v[6])
     if bossHP<=0:
-        print("WIN",playerHP,mana,effects,manaspent,castSpells)
+        print(manaspent,castSpells)
         if manaspent<bestresult:
             bestresult=manaspent
-        return
+        return True
     if bossturn:
         playerHP-=max(1,bossdmg-playerAr)
-        if playerHP<=0:
-            #print("Lose",playerHP,bossHP,mana,manaspent,castSpells)
-            return
-        return fight(bossHP,playerHP,mana,False,stillActiveEffects,manaspent,castSpells)
+        if playerHP>0:
+            return fight(bossHP,playerHP,mana,False,stillActiveEffects,manaspent,castSpells,loseHP)
     else:
+        if loseHP:
+            playerHP-=1
+            if playerHP<=0:
+                return False
         if mana<53:
-            return
+            return False
         for spell in spells:
             if spell[0] in stillActiveEffects.keys() or spell[1]>mana or manaspent+spell[1]>=bestresult:
                 continue
             newEffects = deepcopy(stillActiveEffects)
             newEffects[spell[0]]=spell
-            fight(bossHP,playerHP,mana-spell[1],True,newEffects,manaspent+spell[1],castSpells+"->"+spell[0]+"("+str(mana)+")")
+            fight(bossHP,playerHP,mana-spell[1],True,newEffects,manaspent+spell[1],castSpells+f"->{spell[0]}",loseHP)
 
-fight(init_bossHP,init_playerHP,init_mana,False,{},0,"")
+fight(init_bossHP,init_playerHP,init_mana,False,{},0,"",False)
 print("Task1: ",bestresult)    
-
+bestresult=100000000
+fight(init_bossHP,init_playerHP,init_mana,False,{},0,"",True)
+print("Task2:",bestresult)
     
