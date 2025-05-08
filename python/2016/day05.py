766801890133d8045da66127560e57b52a312240
UrbanCee <urban.cee@gmx.net>
2025-05-09 00:18:05 +0200

    2016 5,6 optimized

--- python/2016/day05.py
+++ python/2016/day05.py
@@ -1,20 +1,16 @@
 import hashlib
 
 current_num=0
-passwd1=""
-passwd2="XXXXXXXX"
-passwd2len=0
-while len(passwd1)<8 or passwd2len<8:
-    md=hashlib.md5()
-    encStr=bytes("uqwqemis"+str(current_num),"utf-8")
-    md.update(encStr)
-    if (md.hexdigest()[0:5]=="00000"):
-        if (len(passwd1)<8): passwd1+=md.hexdigest()[5]
-        pass2pos=int(md.hexdigest()[5],16)
+passwd1,passwd2="","XXXXXXXX"
+
+while len(passwd1)<8 or "X" in passwd2:
+    passw=hashlib.md5(bytes("uqwqemis"+str(current_num),"utf-8")).hexdigest()
+    if (passw[0:5]=="00000"):
+        if (len(passwd1)<8): passwd1+=passw[5]
+        pass2pos=int(passw[5],16)
         if pass2pos>=0 and pass2pos<8 and passwd2[pass2pos]=="X":
-            passwd2=passwd2[:pass2pos]+md.hexdigest()[6]+passwd2[pass2pos+1:]
-            passwd2len+=1
-            print(passwd2)
+            passwd2=passwd2[:pass2pos]+passw[6]+passwd2[pass2pos+1:]
+            print("found new digit: ",passwd2)
     current_num+=1
     
 
--- python/2016/day06.py
+++ python/2016/day06.py
@@ -1,15 +1,11 @@
-
 charInvs =[{},{},{},{},{},{},{},{}]
 
 with open("inputs/day06.txt") as fp:
     for line in list(map(str.strip,fp.readlines())):
         for i in range(len(line)):
-            currDict=charInvs[i]
-            if line[i] not in currDict.keys():
-                currDict[line[i]]=1
-            else:
-                currDict[line[i]]=currDict[line[i]]+1
-            charInvs[i]=currDict
+            charInvs[i][line[i]]=charInvs[i].get(line[i],0)+1
+
+
 
 
 print("Task1: ","".join([sorted([(ch,num) for ch,num in charInv.items()],key=lambda x: x[1],reverse=True)[0][0] for charInv in charInvs]))
