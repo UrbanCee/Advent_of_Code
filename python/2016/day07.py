3127d78ab6f1672b0488e2a807605f9e97e14153
UrbanCee <urban.cee@gmx.net>
2025-05-09 08:46:13 +0200

    day07 almost

--- python/2016/day07.py
+++ python/2016/day07.py
@@ -1,42 +1,16 @@
 import re
 
-exprIn = ["^([a-z]+)\\[","\\]([a-z]+)$","\\]([a-z]+)\\["]
-exprOut = "\\[([a-z]+)\\]"
+exprIn = [re.compile(r'^([a-z]+)\['),re.compile(r'\]([a-z]+)$'),re.compile(r'\]([a-z]+)\[')]
+exprOut = re.compile(r'\[([a-z]+)\]')
 
 def TSL(s):
-    for i in range(len(s)-3):
-        if s[i]==s[i+3] and s[i+1]==s[i+2] and s[i]!=s[i+1]:
-            return True
-    return False
-
+    return sum([s[i]==s[i+3] and s[i+1]==s[i+2] and s[i]!=s[i+1] for i in range(len(s)-3)])>0
 def ABAs(s):
-    babs=[]
-    for i in range(len(s)-2):
-        if s[i]==s[i+2] and s[i]!=s[i+1]:
-            babs.append(s[i+1]+s[i]+s[i+1])
-    return babs
-
-
-TSLcount=0
-SSLcount=0
-
+    return [s[i+1]+s[i]+s[i+1] for i in range(len(s)-2) if s[i]==s[i+2] and s[i]!=s[i+1]]
 
 with open("inputs/day07.txt") as fp:
-    for line in fp.readlines():
-        isTSL=False
-        isSSL=False
-        babs=[]
-        for eIn in exprIn:
-            for addr in re.findall(eIn,line):
-                isTSL=isTSL or TSL(addr)
-                babs.extend(ABAs(addr))
-        for addr in re.findall(exprOut,line):
-            isTSL=isTSL and not TSL(addr)
-            for bab in babs:
-                isSSL = isSSL or (bab in addr)
-        if isTSL: TSLcount+=1
-        if isSSL: SSLcount+=1
+    addresses = [([token  for ex in exprIn for token in ex.findall(line)],[token for token in exprOut.findall(line)]) for line in map(str.strip,fp.readlines())]
 
-print("Task1: ",TSLcount)
-print("Task2: ",SSLcount)
+print("Task1: ",sum([sum([TSL(token) for token in address[0]])>0 and sum([TSL(token) for token in address[1]])==0 for address in addresses]))
+print("Task2: ",sum([ sum([ bab in token for token in address[1] for bab in [ABAs(token) for token in ABAs(address[0])] ])>0 for address in addresses ]))
 
