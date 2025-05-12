0e6d3319de05c122e42a426ebfff90d0d0b367d8
UrbanCee <urban.cee@gmx.net>
2025-05-12 11:16:17 +0200

    day05 updated

--- python/2018/day05.py
+++ python/2018/day05.py
@@ -2,14 +2,11 @@ import re
 with open("inputs/day05.txt") as fp:
     polymer=fp.read().strip()
 
-def reduceStep(poly):
-    reducedPoly,currentPos=[],0
-    while (currentPos<len(poly)):
-        if currentPos==len(poly)-1 or abs(ord(poly[currentPos])-ord(poly[currentPos+1]))!=ord("a")-ord("A"):
-            reducedPoly.append(poly[currentPos])
-            currentPos+=1
-        else: currentPos+=2
-    return "".join(reducedPoly)
+removeGroups=[a+b for x in range(ord("A"),ord("Z")+1) for a,b in [chr(x)+chr(x+ord("a")-ord("A")),chr(x+ord("a")-ord("A"))+chr(x)] ]
+def reduceStep(poly:str):
+    for group in removeGroups:
+        poly=poly.replace(group,"")
+    return poly
 
 def reduce(poly):
     beforestep,afterstep=str(poly),reduceStep(poly)
@@ -20,6 +17,6 @@ def reduce(poly):
     return afterstep
 
 print("Task1 :",len(reduce(polymer)))
-polyGroups=[f"[{chr(x)}{chr(x+ord("a")-ord("A"))}]" for x in range(ord("A"),ord("Z")+1)]
-print("Task2 :",min([len(reduce(re.sub(group,"",polymer))) for group in polyGroups]))
+polyGroups=[re.compile(f"[{chr(x)}{chr(x+ord("a")-ord("A"))}]") for x in range(ord("A"),ord("Z")+1)]
+print("Task2 :",min([len(reduce(group.sub("",polymer))) for group in polyGroups]))
 
