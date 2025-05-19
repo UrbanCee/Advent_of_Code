2b02296fbac6e74cfb79d097bbe418aa67514ffb
UrbanCee <urban.cee@gmx.net>
2025-05-19 10:38:56 +0200

    day11 itertools

--- python/2016/day11_noCheck.py
+++ python/2016/day11_noCheck.py
@@ -1,11 +1,4 @@
-def calc_moves(itemsOnFloors,elevatorMoves = 0):
-    while itemsOnFloors[-1] != sum(itemsOnFloors):
-        for floor in range(0,len(itemsOnFloors)-1):
-            elevatorMoves += 2 * (itemsOnFloors[floor] - 1) - 1
-            itemsOnFloors[floor + 1] += itemsOnFloors[floor]
-            itemsOnFloors[floor] = 0
-    return elevatorMoves
+from itertools import accumulate
 
-
-print("Task1: ",calc_moves( [4, 5, 1, 0]))
-print("Task2: ",calc_moves( [8, 5, 1, 0]))
+print("Task 1:",list(accumulate([(4,0),(5,0),(1,0)],lambda acc,x:(acc[0]+x[0],acc[1]+2*(x[0]+acc[0]-1)-1),initial=(0,0)))[-1][1])
+print("Task 2:",list(accumulate([(8,0),(5,0),(1,0)],lambda acc,x:(acc[0]+x[0],acc[1]+2*(x[0]+acc[0]-1)-1),initial=(0,0)))[-1][1])
