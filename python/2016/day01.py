c01a643a63227c642bee0431309d80611f37cdd4
UrbanCee <urban.cee@gmx.net>
2025-05-23 16:16:46 +0200

    updt

--- python/2016/day01.py
+++ python/2016/day01.py
@@ -4,11 +4,11 @@ from size import *
 with open("inputs/day01.txt") as fp:
     input=fp.read()
 
-expr = "([L|R])(\\d+)"
+expr = re.compile(r'([L|R])(\d+)')
 dirs,dir,visited,pos,firstDoublePos = [(0,1),(1,0),(0,-1),(-1,0)],0,{(0,0)},(0,0),(0,0)
 dirOffset={"R":1,"L":3}
 
-for lr,dist in re.findall(expr,input):
+for lr,dist in expr.findall(input):
     dir = (dir + dirOffset[lr]) % 4
     for i in range(int(dist)):
         pos = vAdd(pos,dirs[dir])
--- python/2016/day06.py
+++ python/2016/day06.py
@@ -6,7 +6,5 @@ with open("inputs/day06.txt") as fp:
             charInvs[i][line[i]]=charInvs[i].get(line[i],0)+1
 
 
-
-
 print("Task1: ","".join([sorted([(ch,num) for ch,num in charInv.items()],key=lambda x: x[1],reverse=True)[0][0] for charInv in charInvs]))
 print("Task2: ","".join([sorted([(ch,num) for ch,num in charInv.items()],key=lambda x: x[1])[0][0] for charInv in charInvs]))
