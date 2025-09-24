e8d23c9e5bf979a2f86b5054a1c33e8a8df5730e
UrbanCee <urban.cee@gmx.net>
2025-09-24 14:37:57 +0200

    17

--- python/2017/day17.py
+++ python/2017/day17.py
@@ -1,1 +1,16 @@
-input=369
+offset=369
+
+pos=1
+buffer = [0,1]
+for i in range(2,2018):
+    buffer.insert(pos,i)
+    pos=(pos+1+offset)%len(buffer)
+print("Task1:", buffer[(pos+len(buffer)-offset)%len(buffer)])
+
+currentIndex=0
+numberPost0=0
+for bufSize in range(1,50000000):
+    currentIndex=(currentIndex+offset+1)%bufSize
+    if (currentIndex==0):
+        numberPost0=bufSize
+print("Task2:",numberPost0)
