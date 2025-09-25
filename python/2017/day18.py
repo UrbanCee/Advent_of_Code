7aaca980608be693cc245ed0f68beff848e956bc
UrbanCee <urban.cee@gmx.net>
2025-09-25 19:48:54 +0200

    18.1

--- python/2017/day18.py
+++ python/2017/day18.py
@@ -2,35 +2,23 @@ import re
 from collections import deque
 
 with open("inputs/day18.txt") as fp:
-    commands = [(cmd,a,b) for line in fp.readlines() for cmd,a,b in re.findall(r'(snd|set|add|mul|mod|rcv|jgz) (-?[a-z0-9]) ?(-?[a-z0-9])?',line)]
+    commands = [(cmd,a,b) for line in fp.readlines() for cmd,a,b in re.findall(r'(snd|set|add|mul|mod|rcv|jgz) (-?[a-z0-9]*) ?(-?[a-z0-9]*)?',line)]
 
 cmdMap = {"set":lambda x ,y: y, "add": lambda x,y:x+y, "mul": lambda x,y:x*y, "mod": lambda x,y:x%y}
 
-def run(pc,r:dict,freq : deque):
-    while pc<len(commands) and pc>=0:
+def evalArg(arg:str,r:dict):
+    return (int(arg) if arg.lstrip("-").isnumeric() else r.get(arg,0))
+
+def run(pc,r:dict,freq:deque):
+    while 0<=pc<len(commands):
         cmd,a,b = commands[pc]
-        if a.lstrip("-").isnumeric():
-            va=int(a)
-        else:
-            va=r.get(a,0)
-        if b.lstrip("-").isnumeric():
-            vb=int(b)
-        else:
-            vb=r.get(b,0)
-        print(r)
-        print(cmd,a,b)
-        print(a,va,b,vb)
         if cmd in cmdMap:
-            r[a]=cmdMap[cmd](va,vb)
+            r[a]=cmdMap[cmd](evalArg(a,r),evalArg(b,r))
         elif cmd == "snd":
-            freq.append(va)
-            print(va)
-        elif cmd == "rcv" and freq[-1]>0:
+            freq.append(evalArg(a,r))
+        elif cmd == "rcv" and r.get(a,0)>0:
             return freq[-1]
-        if cmd == "jgz" and va>0:
-            pc+=vb
-        else:
-            pc+=1
+        pc += evalArg(b,r) if cmd == "jgz" and evalArg(a,r)>0 else 1
 
 
 print("Task 1:",run(0,{},deque()))
