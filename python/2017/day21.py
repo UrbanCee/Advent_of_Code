5daa3fabc926d9592482ac7a21a28dd3ddbbaef2
UrbanCee <urban.cee@gmx.net>
2025-10-20 16:07:18 +0200

    fuuuuuuuu

--- python/2017/day21.py
+++ python/2017/day21.py
@@ -13,17 +13,19 @@ def printstencil(str,w):
     print("\n".join(wrap(str,width=w)))
 def flip2v(str):return str[2:]+str[:2]
 def flip2h(str):return str[1]+str[0]+str[3]+str[2]
+def rotcw2(str):return str[-2::-2]+str[-1::-2]
 def flip3v(str):return str[6:]+str[3:6]+str[:3]
 def flip3h(str):return str[2::-1]+str[5:2:-1]+str[:5:-1]
-def flipdiag(str):return str[8]+str[5]+str[2]+str[1]+str[4]+str[7]+str[6]+str[3]+str[0]
+def rotcw3(str):return str[-3::-3]+str[-2::-3]+str[-1::-3]
 def test2(str,pattern):
-    return str in {pattern,flip2v(pattern),flip2h(pattern),flip2h(flip2v(pattern))}
+    return str in {pattern,flip2v(pattern),flip2h(pattern),flip2h(flip2v(pattern)),rotcw2(pattern),rotcw2(flip2v(pattern)),rotcw2(flip2h(pattern)),rotcw2(flip2h(flip2v(pattern)))}
 def test3(str,pattern):
-    return str in {pattern,flip3v(pattern),flip3h(pattern),flip3h(flip2v(pattern)),flipdiag(pattern),flipdiag(flip3v(pattern)),flipdiag(flip3h(pattern)),flipdiag(flip3h(flip3v(pattern)))}
+    return str in {pattern,flip3v(pattern),flip3h(pattern),flip3h(flip3v(pattern)),rotcw3(pattern),rotcw3(flip3v(pattern)),rotcw3(flip3h(pattern)),rotcw3(flip3h(flip3v(pattern)))}
 def applyRule(pattern,rule,testfunc):
     for key,value in rule.items():
         if testfunc(pattern,key):
             return value
+    print("rule for :",pattern,"not found!")
     return ""
 def extract(field,width,size,tileX,tileY):
     return "".join([field[(tileY*size+i)*width+tileX*size:(tileY*size+i)*width+(tileX+1)*size]for i in range(size)])
@@ -31,19 +33,28 @@ def combine3x3s(tokenarray):
     size=int(math.sqrt(len(tokenarray)))
     return "".join(["".join([tokenarray[tilex+size*tiley][row*3:(row+1)*3] for tilex in range(size)])  for tiley in range(size) for row in range(3)])
 def three2nine(ini3x3):
+#    print("start:")
+#    printstencil(ini3x3,3)
     interm=applyRule(ini3x3,rules3,test3)
+#    print("4x4 step:")
+#    printstencil(interm,4)
     for i in range(2,4):
         interm=combine3x3s([applyRule(extract(interm,2*i,2,x,y),rules2,test2) for y in range(i) for x in range(i)])
-        if i==2: interm_count=interm.count("#")
+ #       print("step",i*3)
+ #       printstencil(interm,3*i)
+        if i==2:
+            interm_count=interm.count("#")
     return (dict(Counter([extract(interm,9,3,x,y) for y in range(3) for x in range(3)])),interm_count)
 
 first3 = three2nine(start)[0]
 count=0
 for key,value in first3.items():
-    print(key,value)
-    count+=value*three2nine(key)[1]
-    print(three2nine(key))
+    print("For: ",key,value)
+    next = three2nine(key)
+    print(next)
+    count+=value*next[1]
 print("Task 1:",count)
+print(rules2)
 
-
-
+#125
+#1782917
