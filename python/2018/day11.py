6ca541a3960bfbb3f829c2eb8f61fb74293967ca
UrbanCee <urban.cee@gmx.net>
2025-10-21 23:47:57 +0200

    2ß16 day11

--- python/2018/day11.py
+++ python/2018/day11.py
@@ -1,23 +1,14 @@
 from size import *
-
 serialNumber = 1723
-
 def power(x,y):
     return ((((x+10)*y+serialNumber)*(x+10))%1000)//100-5
-
-maxpos = (0,0,0)
-max=0
-
-for ra in range(150,151):
-    for y in range(1,301-ra):
-        for x in range(1,301-ra):
-            powersum=0
-            for cy in range(0,ra):
-                for cx in range(0,ra):
-                    powersum+=power(x+cx,y+cy)
-            if powersum > max:
-                max=powersum
-                maxpos=(x,y,ra)
-
-
-print("Task 1: ",maxpos)
+powersums = [power(x,y) for y in range(300) for x in range(300)]
+def boundedValue(x,y):
+    return 0 if y<0 or x<0 else powersums[y*300+x]
+for y in range(300): 
+    for x in range(300):
+        powersums[y*300+x]=powersums[y*300+x]+boundedValue(x,y-1)+boundedValue(x-1,y)-boundedValue(x-1,y-1)
+def findMaxiumSum(offset):
+    return max([(x,y,boundedValue(x-1,y-1)+boundedValue(x+offset,y+offset)-boundedValue(x-1,y+offset)-boundedValue(x+offset,y-1))for y in range(300-offset)for x in range(300-offset)],key = lambda x:x[2])
+print("Task 1: ",findMaxiumSum(2))
+print("Task 2: ",max( [(findMaxiumSum(offset),offset+1) for offset in range(1,100)] , key = lambda x: x[0][2]))
