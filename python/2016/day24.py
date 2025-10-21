a8a7a32aefd8eae5751578bc267cabfee6b1f459
UrbanCee <urban.cee@gmx.net>
2025-10-21 15:55:02 +0200

    24 fin

--- python/2016/day24.py
+++ python/2016/day24.py
@@ -1,7 +1,8 @@
 from size import *
 from queue import PriorityQueue
+import itertools as it
 
-with open("inputs/day24_train.txt") as fp:
+with open("inputs/day24.txt") as fp:
     lines = [line.strip() for line in fp.readlines()]
     s = mySize(len(lines[0]),len(lines))
     playfield="".join(lines)
@@ -11,18 +12,27 @@ with open("inputs/day24_train.txt") as fp:
 def shortestPath(start,end):
     q = PriorityQueue()
     q.put((0,playfield.index(start)))
-    visited=set()
+    queued={playfield.index(start)}
     while not q.empty():
         steps,curr=q.get()
-        visited.add(curr)
         for dir in dirs4:
             newpos=s.addVecToInd(curr,dir)
-            if newpos in visited:
+            if newpos in queued:
                 continue
             if playfield[newpos]==end:
                 return steps+1
             if playfield[newpos]!="#":
+                queued.add(newpos)
                 q.put((steps+1,newpos))
 
 pathLists = {places[i]+places[j]:shortestPath(places[i],places[j]) for i in range(len(places)) for j in range(i+1,len(places)) }
 
+def calcMinS(endAt0):
+    return min( [
+        sum( [ pathLists["".join(sorted([(("0",*way,"0") if endAt0 else ("0",*way))[i],(("0",*way,"0") if endAt0 else ("0",*way))[i+1]]))] 
+              for  i in range(len(way)+1 if endAt0 else len(way)) ] )
+        for way in list(it.permutations([p for p in places if p!="0"]))    
+    ])
+
+print("Task 1:",calcMinS(False))
+print("Task 2:",calcMinS(True))
