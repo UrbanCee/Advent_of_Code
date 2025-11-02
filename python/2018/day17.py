4ab3772edc3962d0f48de8fe669867a8448f71bf
UrbanCee <urban.cee@gmx.net>
2025-11-02 19:56:04 +0100

    day 17 start

--- python/2018/day17.py
+++ python/2018/day17.py
@@ -1,7 +1,45 @@
 import re
+from collections import deque
+with open("inputs/day17.txt") as fp:
+    claycoords = {(int(c1),c) if xy=='x' else (c,int(c1)) for xy,c1,cs,ce in re.findall(r'([xy])=(\d+), [xy]=(\d+)\.\.(\d+)',fp.read().strip()) for c in range(int(cs),int(ce)+1)}
+    rect=((min([x for x,_ in claycoords]),min([y for _,y in claycoords])),(max([x for x,_ in claycoords]),max([y for _,y in claycoords])))
+def printVessels(water):
+    for y in range(rect[0][1],rect[1][1]+1):
+        print("".join(["#" if (x,y) in claycoords else "~" if (x,y) in water else "." for x in range(rect[0][0]-1,rect[1][0]+2)]))
 
 
-with open("inputs/day17_train.txt") as fp:
-    claycoords = [(xy,c1,cs,ce) for xy,c1,cs,ce in re.findall(r'([xy])=(\d+), [xy]=(\d+)\.\.(\d+)',fp.read().strip())]
+def startFill(source,water):
+    for y in range(source[1],rect[1][1]+1):
+        if rect[0][1]<=y<=rect[1][1]:
+            water.add((source[0],y))
+        if (source[0],y+1) in claycoords:
+            return (source[0],y)
+    return (source[0],-1)
+def checkOverflow(source,water,sources):
+    for dir in [1,-1]:
+        for x in [source[0]+offset*dir for offset in range(rect[1][0]-rect[0][0]+2)]:
+            if (x,source[1]) in claycoords:
+                break
+            water.add((x,source[1]))
+            if (x,source[1]+1) not in claycoords and (x,source[1]+1) not in water:
+                sources.append((x,source[1]))
+                break
+
+water=set()
+sources=deque()
+sources.append((500,0))
 
 
+while len(sources)>0:
+    fillPoint = startFill(sources.popleft(),water)
+    if (fillPoint[1]<0): continue #end reached
+    sourcelen=len(sources)
+    while len(sources)==sourcelen:
+        checkOverflow(fillPoint,water,sources)
+        fillPoint=(fillPoint[0],fillPoint[1]-1)
+    print(sources)
+    print("*******************************************************************************")
+    printVessels(water)
+
+
+print("Task 1:",len(water))
