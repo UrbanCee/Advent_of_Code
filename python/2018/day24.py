cecf8c8dbcba8681944278bb95412d6067e92a56
UrbanCee <urban.cee@gmx.net>
2025-11-02 09:14:37 +0100

    day25 done

--- python/2018/day24.py
+++ python/2018/day24.py
@@ -1,8 +1,16 @@
 import re
+from itertools import count
+counter = count(1)
 armyRex = re.compile(r'(\d+) units each with (\d+) hit points (\(.*\))* ?with an attack that does (\d+) (\w+) damage at initiative (\d+)')
 wiRex = re.compile(r'(weak|immune)')
 dmgTypeRex = re.compile(r'(bludgeoning|slashing|fire|radiation|cold)')
 with open("inputs/day24_train.txt") as fp:
-    armies = [[(units,hp,dmg,dmgType,ini,{dmgType:(2 if wi=="weak" else 0) for types in dmgMult.split(";") for wi in wiRex.findall(types) for dmgType in dmgTypeRex.findall(types)}) for units,hp,dmgMult,dmg,dmgType,ini in armyRex.findall(infstr)] for infstr in fp.read().split("Infection:")]
+    armies = [[(next(counter),units,hp,dmg,dmgType,ini,{dmgType:(2 if wi=="weak" else 0) for types in dmgMult.split(";") for wi in wiRex.findall(types) for dmgType in dmgTypeRex.findall(types)}) for units,hp,dmgMult,dmg,dmgType,ini in armyRex.findall(infstr)] for infstr in fp.read().split("Infection:")]
+                #  0      1    2  3     4     5      6     7
+def sortByAttackPower(armies):
+    return [sorted(army,key = lambda g:g[2]*g[4]+g[6]/1000) for army in armies]
 
-print(armies)
+for armyID in range(2):
+    for group in sortByAttackPower(armies[armyID]):
+        for target in armies[1-armyID]:
+            
---
+++ python/2018/day25.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def manDist4(a,b):
+    return sum([abs(a[i]-b[i]) for i in range(len(a))])
+
+with open("inputs/day25.txt") as fp:
+    points = [tuple(map(int, line.strip().split(","))) for line in fp.readlines()]
+
+proxymap = {i:[j for j in range(len(points)) if i != j and manDist4(points[i],points[j]) <=3 ] for i in range(len(points))}
+
+processed = set()
+constellations = list()
+for i in range(len(points)):
+    if i in processed: continue
+    constellation = {i}
+    q = deque()
+    q.extend(proxymap[i])
+    while len(q)>0:
+        p = q.pop()
+        constellation.add(p)
+        q.extend(p1 for p1 in proxymap[p] if p1 not in constellation)
+    constellations.append(constellation)
+    processed = processed.union(constellation)
+
+print("Task 1:",len(constellations))
