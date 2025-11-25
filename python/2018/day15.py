b772989be5686c57446c00453ab02b229610fead
UrbanCee <urban.cee@gmx.net>
2025-11-25 11:35:58 +0100

    15 neighbor search

--- python/2018/day15.py
+++ python/2018/day15.py
@@ -4,8 +4,12 @@ with open("inputs/day15_train1.txt") as fp:
     input = [line.strip() for line in fp.readlines() if len(line.strip())>1]
     s = mySize(len(input[0]),len(input))
     room = [ c for line in input for c in line]
+opp = {'G':'E','E':'G'}
+hp = {i:200 for i,r in enumerate(room) if r in {'G','E'}}
+initiativeOrder = [(r,i) for i,r in enumerate(room) if r in {'G','E'}]
 
-hp = {i:200 for i in range(len(room)) if room[i] in {"G","E"}}
-initiativeOrder = [i for i in range(len(room)) if room[i] in {"G","E"}]
-
+s.print(room)
+for eg,pos in initiativeOrder:
+    if sum(room[s.addVecToInd(pos,dir)]==opp[eg] for dir in dirs4)==0: #no neighbor -> move
+        print(eg,s.toVec(pos))
     
