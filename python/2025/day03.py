3ceba7843066f6947759ad3ebfadb6dfa6bef6df
UrbanCee <urban.cee@gmx.net>
2025-12-03 08:19:11 +0100

    2025 day 3 cmplt

--- python/2025/day03.py
+++ python/2025/day03.py
@@ -1,5 +1,9 @@
-with open("inputs/day03_train.txt") as fp:
+with open("inputs/day03.txt") as fp:
     banks = [[c for c in line.strip()] for line in fp.readlines() if len(line)>1]
 
-print("Task 1:", sum(int(max(bank[:-1])+max(bank[bank.index(max(bank[:-1]))+1:])) for bank in banks))
+def calcMax(arr,i):
+    return max(arr) if i==0 else max(arr[:-i])+calcMax(arr[arr.index(max(arr[:-i]))+1:],i-1)
+
+print("Task 1:", sum(int("".join(calcMax(bank,1))) for bank in banks))
+print("Task 2:", sum(int("".join(calcMax(bank,11))) for bank in banks))
 
