5ef8ef08cf3f36e5279258b7165af04994206251
UrbanCee <urban.cee@gmx.net>
2025-12-05 13:19:13 +0100

    day 05 2

--- python/2025/day05.py
+++ python/2025/day05.py
@@ -1,8 +1,19 @@
 import re
 with open("inputs/day05.txt") as fp:
     input = fp.readlines()
-    ranges = [(int(a),int(b)) for line in input for a,b in re.findall(r"(\d+)\-(\d+)",line)]
+    ranges = list(sorted([(int(a),int(b)) for line in input for a,b in re.findall(r"(\d+)\-(\d+)",line)],key= lambda a:a[0]))
 
 print("Task 1:",sum(sum(i>=a and i<=b for a,b in ranges)>0 for i in [int(i) for line in input for i in re.findall(r"^(\d+)\r?\n",line)]))
-rangeUnion = []
-    
+
+def inInt(v:int,a:tuple[int,int]):return v>=a[0] and v<=a[1]
+rangeUnion,currentRange = [],ranges[0]
+for range in ranges:
+    if inInt(range[0],currentRange):
+        currentRange=(currentRange[0],max(currentRange[1],range[1]))
+    else:
+        rangeUnion.append(currentRange)
+        currentRange=range
+rangeUnion.append(currentRange)
+
+print("Task 2:",sum(b-a+1 for a,b in rangeUnion))    
+
