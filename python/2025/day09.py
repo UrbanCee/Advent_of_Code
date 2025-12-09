ae8fa5581313813ef62b0598d7ae199a59c48e47
UrbanCee <urban.cee@gmx.net>
2025-12-09 10:50:11 +0100

    rects

--- python/2025/day09.py
+++ python/2025/day09.py
@@ -2,5 +2,6 @@ import re
 from itertools import combinations as co
 with open("inputs/day09.txt") as fp:
     points = [(int(x),int(y)) for x,y in re.findall(r'(\d+),(\d+)',fp.read())]
+rects = [((abs(p[0][0]-p[1][0])+1)*(abs(p[0][1]-p[1][1])+1),p) for p in co(points,2)]
 
-print("Task 1:",max((abs(p[0][0]-p[1][0])+1)*(abs(p[0][1]-p[1][1])+1) for p in co(points,2)))
+print("Task 1:",max(rects,key=lambda x:x[0])[0])
