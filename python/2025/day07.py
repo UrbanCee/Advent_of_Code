70fbde4bd88292ff350ddb77d35b9b0fe4251e9d
UrbanCee <urban.cee@gmx.net>
2025-12-09 11:37:14 +0100

    day 7

--- python/2025/day07.py
+++ python/2025/day07.py
@@ -1,26 +1,24 @@
 from size import *
 
-with open("inputs/day07_train.txt") as fp:
+with open("inputs/day07.txt") as fp:
     input = [l.strip() for l in fp.readlines() if len(l)>2]
     s=mySize(len(input[0]),len(input))
     input = [i for l in input for i in l]
 
-input[input.index("S")+s.w]="|"
-for r in range(2,len(input)):
-    for i,c in enumerate(input[r*s.w:(r+1)*s.w]):
-        if (input[s.toInd(i,r-1)]=="|"):
-            if c=="^":
-                input[s.toInd(i-1,r)]="|"
-                input[s.toInd(i+1,r)]="|"
-            if c==".": input[s.toInd(i,r)]="|"
-
-print("Task 1:",sum(input[i-s.w]=="|" for i,c in enumerate(input) if c=="^"))
-
+splits = 0
 knownpaths = dict()
 def countPaths(pos):
-    for i in range(pos//s.w,s.h):
-        if input[pos+i*s.w]=="^":
-            
+    global splits
+    if s.toVec(pos)[1]==s.w-1:
+        return 1
+    if pos in knownpaths: return knownpaths[pos]
+    if input[pos+s.w]=="^":
+        paths=countPaths(pos+s.w+1)+countPaths(pos+s.w-1)
+        splits+=1
+    else:
+        paths=countPaths(pos+s.w)
+    knownpaths[pos]=paths
+    return paths
 
-startPos = input.index("S")+s.w
-
+print("Task 2:",countPaths(input.index("S")))
+print("Task 1:",splits)
--- python/2025/day09.py
+++ python/2025/day09.py
@@ -1,7 +1,15 @@
 import re
 from itertools import combinations as co
-with open("inputs/day09.txt") as fp:
+with open("inputs/day09_train.txt") as fp:
     points = [(int(x),int(y)) for x,y in re.findall(r'(\d+),(\d+)',fp.read())]
 rects = [((abs(p[0][0]-p[1][0])+1)*(abs(p[0][1]-p[1][1])+1),p) for p in co(points,2)]
+horLines = [(pp[1],(min(pp[0],points[i+1][0]),max(pp[0],points[i+1][0]))) for i,pp in enumerate(points) if i <len(points)-1 and pp[1]==points[i+1][1]]
+verLines = [(pp[0],(min(pp[1],points[i+1][1]),max(pp[1],points[i+1][1]))) for i,pp in enumerate(points) if i <len(points)-1 and pp[0]==points[i+1][0]]
+print(verLines)
+
+def notCrossing(r:tuple[tuple[int,int],tuple[int,int]]):
+    return sum()+sum() == 0
+    
 
 print("Task 1:",max(rects,key=lambda x:x[0])[0])
+print("Task 2:",max([r[0] for r in rects if notCrossing(r[1])],key=lambda x:x[0]))
