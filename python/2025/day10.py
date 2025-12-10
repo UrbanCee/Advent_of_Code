9990d85b998e1e20c167ac3b6d4454c51394b69c
UrbanCee <urban.cee@gmx.net>
2025-12-10 08:41:18 +0100

    cntd

--- python/2025/day10.py
+++ python/2025/day10.py
@@ -2,5 +2,11 @@ import re
 with open("inputs/day10_train.txt") as fp:
     input = [([c == '#' for c in lights],[[int(n) for n in button.split(",")] for button in re.findall(r'\(([\d,]+)\)',buttons)],[int(n) for n in joltage.split(",")]) for lights,buttons,joltage in re.findall(r'\[([.#]+)\] (.*) {([\d,]+)}',fp.read())]
 
+def pushbutton(lights,button):
+    return [not s if i in button else s for i,s in enumerate(lights)]
+def tryButtons(lights,buttons):
+    for button in buttons:
+        print(button)
+        for 
+
 for lights,buttons,_ in input:
-    
