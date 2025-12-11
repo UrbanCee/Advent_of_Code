f41e0d31262c6049f84dd1ed8a9252a52aecab05
UrbanCee <urban.cee@gmx.net>
2025-12-11 18:22:07 +0100

    day11 1

--- python/2025/day11.py
+++ python/2025/day11.py
@@ -1,5 +1,11 @@
 import re
-with open("inputs/day11_train.txt") as fp:
+with open("inputs/day11.txt") as fp:
     connections = {node:{con for con in line.strip().split()} for node,line in re.findall("([a-z]+):([a-z ]+)",fp.read())}
 
-print(connections)
+callCache = dict()
+def findPaths(node:str,visited:set[str]):
+    if node in callCache: return callCache[node]
+    if "out" in connections[node]: return 1
+    return sum(findPaths(nextNode,visited.union({node})) for nextNode in connections[node] if nextNode not in visited)
+
+print("Task 1:",findPaths("you",set()))
