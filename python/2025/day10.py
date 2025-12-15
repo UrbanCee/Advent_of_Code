6d59269afe4297293546a8a89186361738b2c362
UrbanCee <urban.cee@gmx.net>
2025-12-15 19:37:21 +0100

    day10

--- python/2025/day10.py
+++ python/2025/day10.py
@@ -1,12 +1,13 @@
 import re
-with open("inputs/day10_train.txt") as fp:
-    input = [([c == '#' for c in lights],[[int(n) for n in button.split(",")] for button in re.findall(r'\(([\d,]+)\)',buttons)],[int(n) for n in joltage.split(",")]) for lights,buttons,joltage in re.findall(r'\[([.#]+)\] (.*) {([\d,]+)}',fp.read())]
+from itertools import combinations as co
+from functools import reduce
+with open("inputs/day10.txt") as fp:
+    inputs = [([c == '#' for c in lights],[[int(n) for n in button.split(",")] for button in re.findall(r'\(([\d,]+)\)',buttons)],[int(n) for n in joltage.split(",")]) for lights,buttons,joltage in re.findall(r'\[([.#]+)\] (.*) {([\d,]+)}',fp.read())]
 
 def pushbutton(lights,button):
     return [not s if i in button else s for i,s in enumerate(lights)]
-def tryButtons(lights,buttons):
-    for button in buttons:
-        print(button)
-        for 
-
-for lights,buttons,_ in input:
+def lowestComb(inp):
+    for i in range(1,len(inp[1])):
+        for comb in co(inp[1],i):
+            if reduce(pushbutton,comb,[False for _ in inp[0]])==inp[0]:return i
+print("Task 1:",sum(lowestComb(input) for input in inputs))
